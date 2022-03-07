f2aac2be3b4fa61056fafe902847f17d71c1bdac
sky92 <zhouhaotian92@126.com>
2022-03-07 22:29:57 +0800

    dd

--- src/shader_cornellbox/shader.glsl.js
+++ src/shader_cornellbox/shader.glsl.js
@@ -35,7 +35,8 @@ export const fragment = /*glsl*/`
     uniform float speculerRatio;
     uniform float diffuseRatio;
 
-    uniform vec3 lightPos;
+    vec3 lightPos;
+    vec3 lightColor;
 
     varying vec3 objectNormal;
 
@@ -44,14 +45,33 @@ export const fragment = /*glsl*/`
 
     void main(){
  
+      
         vec4 color=vec4(vColor);
 
         //color.r += sin(vPosition.x*10.0+time)*0.5 ;
 
+        lightPos = vec3(20.0,20.0,20.0);
+        lightColor = vec3(1.0,1.0,1.0);
         vec3 norm = normalize(objectNormal);
+        vec3 lightDir = normalize(lightPos - vPosition);
+
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse =diffuseRatio * diff * lightColor;
   
         vec4 ambient = ambientRatio * color;
 
-        gl_FragColor = ambient;
+        gl_FragColor = ambient +vec4(diffuse,1.0) ;
     }
 `;
+
+
+
+/*
+vac3 transformed = vec3(position);
+vec3 objectNormal = vec3(normalize(objectNormal));
+
+varying ve3c3 vColor;
+
+vColor.xyz = color.xyz;
+
+*/
